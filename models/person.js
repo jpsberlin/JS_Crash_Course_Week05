8b76e8390939c3d327366c1d54c85f431730beb5
jpsberlin <jpsberlin@hotmail.com>
2018-11-11 12:17:58 +0100

    Delete person.js

--- models/person.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-const PersonSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-})
-
-module.exports = mongoose.model('Person', PersonSchema);
